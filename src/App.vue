a86b41769508cf7e32937bc5540c20dbc1f4d47a
Muhammad Awais <muhammadawais6640@gmail.com>
2024-06-11 11:01:33 +0500

    redirect liqueous app

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <NcAppContent>
+    <div id="liqueous">
+      <h1>Hello world!</h1>
+      <a href="https://www.liqueous.com">Redirect to Liqueous</a>
+    </div>
+  </NcAppContent>
+</template>
+
+<script>
+import NcAppContent from "@nextcloud/vue/dist/Components/NcAppContent.js";
+
+export default {
+  name: "App",
+  components: {
+    NcAppContent,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+#liqueous {
+  display: flex;
+  justify-content: center;
+  margin: 16px;
+}
+</style>
